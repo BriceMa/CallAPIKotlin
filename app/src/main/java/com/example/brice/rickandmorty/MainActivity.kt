38fcfe3ce3ae49c7c8ed48354185b5d4d084557e
Brice <>
2019-01-29 16:08:06 +0100

    finish non functional

--- app/src/main/java/com/example/brice/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/brice/rickandmorty/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.brice.rickandmorty
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.brice.rickandmorty.network.EpisodeService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        EpisodeService.getEpisodes({episodes ->
+            episodes.size
+        },{
+
+        })
     }
 }
--- app/src/main/java/com/example/brice/rickandmorty/network/EpisodeService.kt
+++ app/src/main/java/com/example/brice/rickandmorty/network/EpisodeService.kt
@@ -4,6 +4,7 @@ import android.hardware.camera2.CaptureFailure
 import com.android.volley.NetworkResponse
 import com.android.volley.Request
 import com.android.volley.VolleyError
+import com.example.brice.rickandmorty.RickAndMortyApp
 import com.example.brice.rickandmorty.response.Episode
 import com.example.brice.rickandmorty.response.ResultEpisode
 import com.neopixl.spitfire.listener.RequestListener
@@ -11,26 +12,28 @@ import com.neopixl.spitfire.request.BaseRequest
 
 class EpisodeService {
     companion object {
-        fun getEpisodes(success: (episodes: List<Episode>) -> Unit,
-                        failure: (error: VolleyError?) -> Unit){
+        fun getEpisodes(
+            success: (episodes: List<Episode>) -> Unit,
+            failure: (error: VolleyError?) -> Unit
+        ) {
 //          1 - Récupération de l'URL
 
-            val url:String = UrlBuilder.episodesUrl
+            val url: String = UrlBuilder.episodesUrl
 
 //          2 - Call HTTP
 //          3 - Parsing JSON
 //          4 - Envoi des données (Callback)
 
-            val request
-                    = BaseRequest.Builder<ResultEpisode>(Request.Method.GET, url, ResultEpisode::class.java)
-                .listener(object: RequestListener<ResultEpisode>{
+            val request = BaseRequest.Builder<ResultEpisode>(Request.Method.GET, url, ResultEpisode::class.java)
+                .listener(object : RequestListener<ResultEpisode> {
 
                     override fun onSuccess(
                         request: Request<ResultEpisode>,
                         response: NetworkResponse,
                         result: ResultEpisode?
                     ) {
-                        val episode = result?.results ?: emptyList()
+                        val episodes = result?.results ?: emptyList()
+                        success(episodes)
                     }
 
                     override fun onFailure(
@@ -38,10 +41,13 @@ class EpisodeService {
                         response: NetworkResponse?,
                         error: VolleyError?
                     ) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                        failure(error)
                     }
 
-                })
+                }).build() // pour céer l'objet request
+
+            // Lancement de la requête
+            RickAndMortyApp.resquestQueue.add(request)
 
 //          5 - Stockage des données dans une BDD locale (optionnel)
         }
